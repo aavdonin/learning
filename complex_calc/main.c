103f59d608188394223b8e5393e6f3ab8407809c
Alexey Avdonin <a.e.avdonin@gmail.com>
2018-10-23 22:37:53 +0700

    dynamic lib usage refactoring

--- complex_calc/main.c
+++ complex_calc/main.c
@@ -1,68 +1,37 @@
 #include <stdio.h>
 #include <dlfcn.h>
 #include "lib.h"
+#define LIB_COUNT 4
 
 int main(void) {
   int menu_item;
   struct complex a, b;
-  //list plugin folder ??
-  //load existing libs
-  void *libadd, *libsub, *libmul, *libdiv;
-  libadd = dlopen("plugin/libadd.so",RTLD_LAZY);
-  if (!libadd) fprintf(stderr, "dlopen error: %s\n", dlerror());
-  libsub = dlopen("plugin/libsub.so",RTLD_LAZY);
-  if (!libsub) fprintf(stderr, "dlopen error: %s\n", dlerror());
-  libmul = dlopen("plugin/libmul.so",RTLD_LAZY);
-  if (!libmul) fprintf(stderr, "dlopen error: %s\n", dlerror());
-  libdiv = dlopen("plugin/libdiv.so",RTLD_LAZY);
-  if (!libdiv) fprintf(stderr, "dlopen error: %s\n", dlerror());
-  //get symbols for operation funcs
-  struct complex (*add)(struct complex, struct complex);
-  struct complex (*sub)(struct complex, struct complex);
-  struct complex (*mul)(struct complex, struct complex);
-  struct complex (*div)(struct complex, struct complex);
-  add = dlsym(libadd,"add");
-  if (!add) fprintf(stderr, "dlsym error: %s\n", dlerror());
-  sub = dlsym(libsub,"sub");
-  if (!sub) fprintf(stderr, "dlsym error: %s\n", dlerror());
-  mul = dlsym(libmul,"mul");
-  if (!mul) fprintf(stderr, "dlsym error: %s\n", dlerror());
-  div = dlsym(libdiv,"div");
-  if (!div) fprintf(stderr, "dlsym error: %s\n", dlerror());
-  //draw menu according to found symbols
+  char *func_names[] = {"add","sub","mul","div",};
+  char *lib_locs[] = {"plugin/libadd.so","plugin/libsub.so","plugin/libmul.so","plugin/libdiv.so",};
+  void *libs[LIB_COUNT];
+  struct complex (*funcs[LIB_COUNT])(struct complex, struct complex);
+  int i;
+  for (i=0;i<LIB_COUNT;i++) {   //open libs and get symbols for functions
+      libs[i] = dlopen(lib_locs[i],RTLD_LAZY);
+      funcs[i] = dlsym(libs[i],func_names[i]);
+  };
   for (menu_item = -1;menu_item != '5';) {
-    printf("1)%s\n2)%s\n3)%s\n4)%s\n5)Quit\n",add?"Add":"",sub?"Sub":"",mul?"Mul":"",div?"Div":"");
+    for (i=0;i<LIB_COUNT;i++) { //draw menu according to found symbols
+      printf("%i)%s\n",i+1,funcs[i]?func_names[i]:"");
+    };
+    printf("5)quit\n");
     while ((menu_item = getchar()) == 10);
     if (menu_item < '1' || menu_item > '5') {	//validate input
       printf("You entered wrong number, try again...\n");
       continue;
+    }
+    else {
+      menu_item = menu_item - '0';  // char '2' -> int 2, etc
     };
-    if (menu_item == '5') break;
-    switch (menu_item) {	//do what it says
-      case '1': ;	//Add
-        if (add) {
-            get_numbers(&a,&b);
-            print_result((*add)(a,b));
-        }
-        break;
-      case '2':         //Sub
-        if (sub) {
-            get_numbers(&a,&b);
-            print_result((*sub)(a,b));
-        }
-        break;
-      case '3':         //Mul
-        if (mul) {
-            get_numbers(&a,&b);
-            print_result((*mul)(a,b));
-        }
-        break;
-      case '4':         //Div
-        if (div) {
-            get_numbers(&a,&b);
-            print_result((*div)(a,b));
-        }
-        break;
+    if (menu_item == 5) break;
+    else if (funcs[menu_item-1]) {  //if chosen function loaded
+        get_numbers(&a,&b);
+        print_result((*funcs[menu_item-1])(a,b));   //call it and print result
     };
   };
   return 0;
