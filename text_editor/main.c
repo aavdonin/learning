ccf79ef10f69446af6fe882b42e0662920a2b40c
Alexey Avdonin <a.e.avdonin@gmail.com>
2018-10-31 14:29:41 +0700

    formatting: replace tab by 4 spaces

--- text_editor/main.c
+++ text_editor/main.c
@@ -9,34 +9,34 @@ char filename[FILENAME_MAX_LEN];
 int main(void) {
     int menu_item = -1;
     char **bufptr;
-	char *buf;
-	bufptr = &buf;
+    char *buf;
+    bufptr = &buf;
     long bufsize, bufused;
     initscr();
-	noecho();
+    noecho();
     if (!has_colors()) {
         endwin();
         printf("There's no colors here :(\n");
         return 1;
     }
     start_color();
-	init_pair(1, COLOR_RED, COLOR_BLACK);
-	init_pair(2, COLOR_GREEN, COLOR_BLACK);
-	while (menu_item != '5') {
-		if (menu_item != '3') 
-			print_menu();
-		menu_item = getch();
-		switch (menu_item) {
-			case '1':		//open file
-				if (file) break;
-				attron(COLOR_PAIR(2));
-				mvprintw(1, 8, "Filename: ");
-				refresh();
-				echo();
-				attroff(COLOR_PAIR(2));
-				getstr(filename);
-				noecho();
-				file = fopen(filename, "r");
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    while (menu_item != '5') {
+        if (menu_item != '3') 
+            print_menu();
+        menu_item = getch();
+        switch (menu_item) {
+            case '1':        //open file
+                if (file) break;
+                attron(COLOR_PAIR(2));
+                mvprintw(1, 8, "Filename: ");
+                refresh();
+                echo();
+                attroff(COLOR_PAIR(2));
+                getstr(filename);
+                noecho();
+                file = fopen(filename, "r");
                 if (!file) {
                     attron(COLOR_PAIR(1));
                     mvprintw(1, 8, "Can't open file!");
@@ -45,51 +45,51 @@ int main(void) {
                     break;
                 }
                 bufsize = load_text(file, bufptr);
-				break;
-			case '2':		//edit
+                break;
+            case '2':        //edit
                 if (file) {
                     clear();
                     mvprintw(0,0,"%s",buf);
                     refresh();
                     getch();
                 }
-				break;
-			case '3':		//save
-				if (file) {
-					attron(COLOR_PAIR(2));
-					mvprintw(3, 8, "OK!");
-					attroff(COLOR_PAIR(2));
-					move(6, 0);
-				}
-				break;
-			case '4':		//close
+                break;
+            case '3':        //save
+                if (file) {
+                    attron(COLOR_PAIR(2));
+                    mvprintw(3, 8, "OK!");
+                    attroff(COLOR_PAIR(2));
+                    move(6, 0);
+                }
+                break;
+            case '4':        //close
                 if (file) {
                     fclose(file);
                     file = NULL;
                     *filename = '\0';
                     free(buf);
                 }
-				break;
-		}
-	}
+                break;
+        }
+    }
     endwin();
     return 0;
 }
 
 void print_menu(void) {
-	clear();
-	printw("Text editor\n");
-	printw("1) Open");
-	if (file) {
-		printw("ed: ");
-		attron(COLOR_PAIR(1));
-		printw("%s\n", filename);
-		attroff(COLOR_PAIR(1));
-	}
-	else printw("\n");
-	printw("2)%s", file ? " Edit\n" : "\n");
-	printw("3)%s", file ? " Save\n" : "\n");
-	printw("4)%s", file ? " Close\n" : "\n");
-	printw("5) Exit\n");
-	refresh();
+    clear();
+    printw("Text editor\n");
+    printw("1) Open");
+    if (file) {
+        printw("ed: ");
+        attron(COLOR_PAIR(1));
+        printw("%s\n", filename);
+        attroff(COLOR_PAIR(1));
+    }
+    else printw("\n");
+    printw("2)%s", file ? " Edit\n" : "\n");
+    printw("3)%s", file ? " Save\n" : "\n");
+    printw("4)%s", file ? " Close\n" : "\n");
+    printw("5) Exit\n");
+    refresh();
 }
