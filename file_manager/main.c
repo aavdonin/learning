739d558fdd136bea1121cefc66ba5a4863f09f5c
Alexey Avdonin <a.e.avdonin@gmail.com>
2018-11-09 17:44:05 +0700

    directory listing

--- file_manager/main.c
+++ file_manager/main.c
@@ -1,13 +1,36 @@
 #include <ncurses.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
 #include "defines.h"
 
+static int filter_func(const struct dirent *entry) {
+    return strcmp(entry->d_name,".");
+}
+
 int main(void) {
+    int num, i;
+    struct dirent **entry;
     initscr();
     noecho();
-    cbreak();
     keypad(stdscr, TRUE);
-    if (has_colors()) 
-        start_color();
+
+    printw("Soon I'll become a file manager!\n");
+    num = scandir(".", &entry, filter_func, alphasort);
+    if (num<0) {
+        printw("Directory read error");
+        getch();
+        endwin();
+        return 1;
+    }
+    for (i = 0; i < num; i++) {
+        printw("%20s inode:%i\n", entry[i]->d_name, entry[i]->d_ino);
+        free(entry[i]);
+    }
+    free(entry);
+
+    refresh();
     getch();
+    endwin();
     return 0;
 }
