25cf513eeb997e6c615ac8afa032992286986537
Alexey Avdonin <a.e.avdonin@gmail.com>
2018-11-21 22:53:10 +0700

    myshell initial commit

---
+++ myshell/parser.c
@@ -0,0 +1,6 @@
+#include <string.h>
+
+void *parse_cmd(char *cmd) {
+    //function to parse given string with strtok to a set of substrings
+    //allocates memory for substrings array and returns pointer to it
+}
