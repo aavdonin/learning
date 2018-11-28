0fb8290a84200281e47153eb25fab3bf9c775a9d
Alexey Avdonin <a.e.avdonin@gmail.com>
2018-11-28 17:44:56 +0700

    reg/unreg clients in separate thread

--- chat/server/server.c
+++ chat/server/server.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
@@ -8,48 +9,32 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
-#include <time.h>
 #include "../defines.h"
 #include "array_fnc.h"
 
 void send_msg(int qid, int *clients, char *msg);
+void *hostess(void *arg);
+
+int *clients = NULL;
 
 int main(int argc, char *argv[]) {
     int msqid;
-    message msg_in, msg_reg;
+    message msg_in;
     if ((msqid = msgget(MSGQUEUEKEY, IPC_CREAT | 0777)) < 0) {
         perror("msgget");
         return 1;
     }
-    int *clients = NULL;
-    while (1) {
-        memset(msg_reg.mtext,0,MSGSIZE);
-        if (msgrcv(msqid, &msg_reg, MSGSIZE, 1, IPC_NOWAIT) < 0) {
-            if (errno != ENOMSG)
-                perror("msgrcv(reg)");
-        }
-        else {
-            char msg[MSGSIZE];
-            int cl_pid = atoi(msg_reg.mtext);
-            if (cl_pid > 0) {
-                //received new connection
-                printf("Client pid <%d> connected\n", cl_pid);
-                //add client pid to array
-                clients = append(clients, cl_pid);
-                //send message about new client
-                sprintf(msg, "User %d joined us!", cl_pid);
-            }
-            else {
-                //client disconnected
-                printf("Client pid <%d> disconnected\n", -cl_pid);
-                del(clients, -cl_pid);
-                sprintf(msg, "User %d leaves chat", -cl_pid);
-            }
-            send_msg(msqid, clients, msg);
-        }
-
+    //run hostess thread
+    pthread_t t_hostess;
+    int status;
+    status = pthread_create(&t_hostess, NULL, hostess, (void *)&msqid);
+    if (status != 0) {
+        perror("Unable to create 'hostess' thread!\n");
+        return 1;
+    }
+    while (1) { //main cycle
         memset(msg_in.mtext,0,MSGSIZE);
-        if (msgrcv(msqid, &msg_in, MSGSIZE, 2, IPC_NOWAIT) < 0) {
+        if (msgrcv(msqid, &msg_in, MSGSIZE, 2, 0) < 0) {
             if (errno != ENOMSG)
                 perror("msgrcv(in)");
         }
@@ -73,8 +58,37 @@ void send_msg(int qid, int *clients, char *msg) {
         strcpy(msg_out.mtext, msg);
         if (msgsnd(qid, &msg_out, strlen(msg), IPC_NOWAIT) < 0) {
             perror("msgsnd");
-            //todo: register and unregister clients in separate thread
-            //make blocking queue reads
+        }
+    }
+}
+
+void *hostess(void *arg) {
+    int *qid = (int*) arg;
+    message msg_reg;
+    memset(msg_reg.mtext,0,MSGSIZE);
+    while (1) {
+        if (msgrcv(*qid, &msg_reg, MSGSIZE, 1, 0) < 0) {
+            if (errno != ENOMSG)
+                perror("msgrcv(reg)");
+        }
+        else {
+            char msg[MSGSIZE];
+            int cl_pid = atoi(msg_reg.mtext);
+            if (cl_pid > 0) {
+                //received new connection
+                printf("Client pid <%d> connected\n", cl_pid);
+                //add client pid to array
+                clients = append(clients, cl_pid);
+                //send message about new client
+                sprintf(msg, "User %d joined us!", cl_pid);
+            }
+            else {
+                //client disconnected
+                printf("Client pid <%d> disconnected\n", -cl_pid);
+                del(clients, -cl_pid);
+                sprintf(msg, "User %d leaves chat", -cl_pid);
+            }
+            send_msg(*qid, clients, msg);
         }
     }
 }
