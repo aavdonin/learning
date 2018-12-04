6ebbd9e922b42583e7e710f97c0026821d24a874
Alexey Avdonin <a.e.avdonin@gmail.com>
2018-12-04 22:58:42 +0700

    tcp echo server prototype

--- echo/server.c
+++ echo/server.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <errno.h>
 
 #define PORT 7777
@@ -8,34 +13,36 @@
 #define BUFSIZE 1024
 
 int main (int argc, char *argv[]) {
-    struct sockaddr_storage peer_addr;
-    socklen_t addr_size;
-    struct addrinfo hints, *res;
     int sock_tcp; //socket file descriptor
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;     // automatically fill IP
-    if (getaddrinfo(NULL, PORT, &hints, &res) != 0) {
-        perror("getaddrinfo");
-        return 1;
-    }
-    sock_tcp = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    struct sockaddr_in addr;
+    char buf[BUFSIZE];
+
+    sock_tcp = socket(AF_INET, SOCK_STREAM, 0);
     if (sock_tcp < 0) {
         perror("Socket creation error");
         return 1;
     }
     
-    if (bind(sock_tcp, res->ai_addr, res->ai_addrlen) != 0) {
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    if (bind(sock_tcp, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
         perror("Socket binding error");
         return 1;
     }
     listen(sock_tcp, BACKLOG);
 
-    addr_size = sizeof(peer_addr);
-    new_fd = accept(sock_tcp, (struct sockaddr *)&peer_addr, &addr_size);
-    
-    //receive message
-    //send back
-    //close socket
+    while (1) {
+        int new_sock;
+        struct sockaddr_in peer_addr;
+        int addr_size = sizeof(peer_addr);
+        new_sock = accept(sock_tcp, (struct sockaddr *)&peer_addr, &addr_size);
+        printf("Incoming connection: %s:%d\n", inet_ntoa(peer_addr.sin_addr),
+        ntohs(peer_addr.sin_port));
+
+        send(new_sock, buf, recv(new_sock, buf, BUFSIZE, 0), 0);
+        close(new_sock);
+        printf("disconnected\n");
+    }
+
 }
