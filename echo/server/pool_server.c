c97360a95f7de5bc3589c0a1a06f45fba296b673
Alexey Avdonin <a.e.avdonin@gmail.com>
2019-01-04 16:55:06 +0700

    handler thread finished
    added code for reading message from socket and answering back;
    fixed storage of sockets descriptors within thread;

--- echo/server/pool_server.c
+++ echo/server/pool_server.c
@@ -86,6 +86,7 @@ int main(int argc, char *argv[]) {
 void *handler_thread(void *arg) {
     thread_info_t *thread_info = (thread_info_t *) arg;
     struct pollfd fds[MAX_THREAD_CLIENTS+1];
+    int i;
     fds[0].fd = thread_info->pipe[0]; //will listen to pipe's output
     fds[0].events = POLLIN;
     for (i=1; i<=MAX_THREAD_CLIENTS; i++)
@@ -97,20 +98,45 @@ void *handler_thread(void *arg) {
         else if (ret == 0) {
             continue;
         }
-        if (fds[0].revents & POLLIN) {
+        if (fds[0].revents & POLLIN) {//received new socket descriptor from pipe
             fds[0].revents = 0;
             int new_sock;
             if (read(fds[0], new_sock, sizeof(new_sock)) <= 0) {
                 perror("thread read from pipe err");
-                continue;
             }
-            fds[++thread_info->cl_cnt].fd = new_sock; //don't rely on cl_cnt here
-            fds[thread_info->cl_cnt].events = POLLIN; //it's wrong after disconnecting a client
+            else {
+                //add that socket into watch list
+                for (i=1; i<=MAX_THREAD_CLIENTS; i++) {
+                    if (fds[i].fd < 0) {
+                        fds[i].fd = new_sock;
+                        fds[i].events = POLLIN;
+                        ++thread_info->cl_cnt;
+                        break;
+                    }
+                }
+            }
         }
-        int i;
+
         for (i=1; i<=MAX_THREAD_CLIENTS; i++) {
             if (fds[i].revents & POLLIN) {
                 //read and answer to socket
+                char buf[BUFSIZE];
+                ssize_t msglen;
+                if ((msglen = recv(fds[i].fd, buf, BUFSIZE, 0)) < 0) {
+                    perror("recv failed");
+                    continue;
+                }
+                else if (msglen == 0) {
+                    printf("client closed connection\n");
+                    close(fds[i].fd);
+                    fds[i].fd *= -1; //disable listening on this fd
+                    --thread_info->cl_cnt;
+                    continue;
+                }
+                if (send(fds[i].fd, buf, msglen, 0) < 0) {
+                    perror("send failed");
+                    continue;
+                }
             }
         }
     }
